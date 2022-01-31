f3932844ed04a004606cbe78c01c5e8b9f97b32b
Lev Goldenberg <zxcv02@gmail.com>
2022-01-31 15:12:17 +0200

    Scores Handeling #2

--- Score.py
+++ Score.py
@@ -1,5 +1,4 @@
 import os.path
-
 from Utils import SCORES_FILE_NAME
 
 
@@ -7,10 +6,10 @@ def add_score(difficulty):
     scores_file = open(SCORES_FILE_NAME, 'a')
     scores_size = os.path.getsize("Scores.txt")
     points_of_winning = int(difficulty)*3 + 5
-    if scores_size == 0:
-        scores_file.write(str(points_of_winning))
-    else:
+    if scores_size > 0:
         scores_file.write(', ' + str(points_of_winning))
+    else:
+        scores_file.write(str(points_of_winning))
 
 
 
